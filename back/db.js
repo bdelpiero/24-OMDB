8526eafc216095463ca66e576373b0532f5418a5
Bruno Del Piero <bdelpiero93@gmail.com>
2020-12-06 20:25:54 -0300

    modificaciones al back

--- back/db.js
+++ back/db.js
@@ -1,16 +1,23 @@
 const Sequelize = require("sequelize");
+const config = require("../config/config.json");
 let db;
 
-if (process.env.DATABASE_URL) {
-  db = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "postgres",
-    protocol: "postgres",
-    port: 5432,
-    host: "<heroku host>",
-    logging: false,
-  });
+if (config.use_env_variable) {
+  db = new Sequelize(process.env[config.use_env_variable]);
 } else {
   db = new Sequelize("postgres://localhost/omdb", { logging: false });
 }
+
+// if (process.env.DATABASE_URL) {
+//   db = new Sequelize(process.env.DATABASE_URL, {
+//     dialect: "postgres",
+//     protocol: "postgres",
+//     port: 5432,
+//     host: "<heroku host>",
+//     logging: false,
+//   });
+// } else {
+//   db = new Sequelize("postgres://localhost/omdb", { logging: false });
+// }
 
 module.exports = db;
